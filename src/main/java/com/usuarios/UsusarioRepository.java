56f50699f010bcf3efdb12aa76f7bb0d1680b37f
GGabriel042 <gabrielgonzaga042@gmail.com>
2026-04-28 16:32:12 -0300

    Usuario Repository

--- src/main/java/com/usuarios/UsusarioRepository.java
+++ src/main/java/com/usuarios/UsusarioRepository.java
@@ -1,7 +1,10 @@
 package com.usuarios;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UsusarioRepository extends JpaRepository<Usuario, Long> {
+
+    UserDetails findByUsername(String username);
     
 }
